4f61ed736b2296cba854869ee1be2b6c20f5abc0
subash-001 <subashs10011@gmail.com>
2024-06-04 15:09:41 +0530

    first commit

---
+++ Rec1.java
@@ -0,0 +1,17 @@
+public class Rec1 {
+    public static void main(String[] args) {
+        fun(5);
+        
+    }
+
+    static void  fun(int n){
+        if(n==0){
+            return;
+        }
+
+        fun(n-1);
+        System.out.println(n);
+
+    }
+
+}
