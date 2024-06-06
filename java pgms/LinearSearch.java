bda706a6fdd89705d442cbf01d4577e2d9946ea6
Subash <subashs10011@gmail.com>
2024-06-06 23:27:20 +0530

    Linear search that returns ArrayList

--- java pgms/LinearSearch.java
+++ java pgms/LinearSearch.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 // Using recursion
 public class LinearSearch {
@@ -6,23 +7,40 @@ public class LinearSearch {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter target");
         int target= sc.nextInt();
+        System.out.println(findList(arr, target, 0, list));
+        // System.out.println(search(arr, target, 0));
+        // System.out.println(findElement(arr, target, 0));
+
+    }
+
+    // static int search(int[] arr, int target, int index){
+    //     if(index== arr.length) return -1;
+    //     if(arr[index]== target) return index;
+    //     return search(arr, target, index+1);
+    // }
+
+    // static Boolean findElement(int[] arr, int target, int index){
+    //     if(index== arr.length){
+    //         return false;
+    //     }
+    //     return arr[index] == target || findElement(arr, target, index+1);
+    // }
+
+        // Linear search that returns an ArrayList
+    static ArrayList<Integer> list = new ArrayList<>();
+   
+    static ArrayList<Integer> findList(int[] arr, int target, int index,ArrayList<Integer> list){
         
-        System.out.println(search(arr, target, 0));
-        System.out.println(findElement(arr, target, 0));
-
-    }
-
-    static int search(int[] arr, int target, int index){
-        if(index== arr.length) return -1;
-        if(arr[index]== target) return index;
-        return search(arr, target, index+1);
-    }
-
-    static Boolean findElement(int[] arr, int target, int index){
         if(index== arr.length){
-            return false;
+            return list;
         }
-        return arr[index] == target || findElement(arr, target, index+1);
+        if(arr[index]== target){
+            list.add(index);
+        }
+        return findList(arr, target, index+1, list);
     }
+
+
+
 
 }
