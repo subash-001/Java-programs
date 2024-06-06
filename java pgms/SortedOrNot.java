3e617fd20e63824bf56af6b7d8f50c5555fcc4c2
Subash <subashs10011@gmail.com>
2024-06-06 17:02:53 +0530

    array sorting

---
+++ java pgms/SortedOrNot.java
@@ -0,0 +1,12 @@
+public class SortedOrNot {
+    public static void main(String[] args) {
+        int[] arr ={1,3,3,33,99,89};
+        int index=0;
+       System.out.println(sorted(arr,  index)); 
+    }
+    static Boolean sorted(int[] arr, int index){
+        if(index== arr.length-1) return true;
+        return (arr[index]<= arr[index+1]) && sorted(arr, index+1);
+
+    }
+}
