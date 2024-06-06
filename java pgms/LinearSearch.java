882fc13ff1bee5d93a93d6b7eaee22a41a33cc91
Subash <subashs10011@gmail.com>
2024-06-06 17:41:36 +0530

    linear seach rec

---
+++ java pgms/LinearSearch.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+// Using recursion
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = {3,55,6,55,33,6};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter target");
+        int target= sc.nextInt();
+        
+        System.out.println(search(arr, target, 0));
+
+    }
+
+    static int search(int[] arr, int target, int index){
+        if(index== arr.length) return -1;
+        if(arr[index]== target) return index;
+        return search(arr, target, index+1);
+    }
+}
