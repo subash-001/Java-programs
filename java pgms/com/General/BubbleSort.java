413dc1b0ed02ef235ce86b512e3d9d7f6565b191
Subash <subashs10011@gmail.com>
2024-06-12 18:13:54 +0530

    .

--- java pgms/com/General/BubbleSort.java
+++ java pgms/com/General/BubbleSort.java
@@ -11,20 +11,19 @@ public class BubbleSort {
     }
 
     static void Sort(int[] arr){
-        Boolean swap;
+        
         for (int i=0;i<arr.length;i++){
-           swap= false;
+           
             for (int j=1;j<arr.length-i;j++){
                 if(arr[j]<arr[j-1]){
+                    
                     int temp= arr[j];
                     arr[j]= arr[j-1];
                     arr[j-1]= temp;
-                    swap=true;
+                    
                 }
             }
-            if(!swap){
-                break;
-            }
+            
         }
     }
 }
