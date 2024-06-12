bb51f111259a4fae584ff1f1e2c1e3e5ef9706ee
Subash <subashs10011@gmail.com>
2024-06-12 18:18:23 +0530

    boolean for swap

--- java pgms/com/General/BubbleSort.java
+++ java pgms/com/General/BubbleSort.java
@@ -11,19 +11,24 @@ public class BubbleSort {
     }
 
     static void Sort(int[] arr){
-        
+        Boolean swap;
         for (int i=0;i<arr.length;i++){
-           
+           swap= false;
+           //for each iteration the max item come at the last respective index
             for (int j=1;j<arr.length-i;j++){
                 if(arr[j]<arr[j-1]){
-                    
+                    //swap
                     int temp= arr[j];
                     arr[j]= arr[j-1];
                     arr[j-1]= temp;
-                    
+                    swap=true;
                 }
             }
-            
+            // if you didn't swap for a particular value of i, it means that the array is sorted
+            //hence break the loop
+            if(!swap){
+                break;
+            }
         }
     }
 }
