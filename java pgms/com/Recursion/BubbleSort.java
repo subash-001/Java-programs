8ab69b22062a028113ac6a1a84a430dc0a47822e
Subash <subashs10011@gmail.com>
2024-06-13 00:06:57 +0530

    rec

--- java pgms/com/Recursion/BubbleSort.java
+++ java pgms/com/Recursion/BubbleSort.java
@@ -5,28 +5,24 @@ import java.util.Arrays;
 public class BubbleSort {
     public static void main(String[] args) {
         int[] arr ={4,3,2,1};
-        Sort(arr,0, arr.length-1);
-        System.out.println(Arrays.toString(arr));
+       Sort(arr, arr.length-1);
+       System.out.println(Arrays.toString(arr));
         
     }
-    static void Sort(int[] arr, int i, int j){
-        if(j==0){
+    static void Sort(int[] arr, int n){
+        
+        if(n==0){
             return ;
         }
-
-        if(i<j){
-            if(arr[i]> arr[i+1]){
-                //swap
-                int temp = arr[i];
+        for(int i=0;i<n;i++){
+            if(arr[i]>arr[i+1]){
+                int temp= arr[i];
                 arr[i]= arr[i+1];
-                arr[i+1]=temp;
+                arr[i+1] = temp;
             }
-            Sort(arr, i+1, j);
         }
-        else{
-            Sort(arr, i, j-1);
-        }
-        
+        Sort(arr, n-1);
+               
 
     }
 }
