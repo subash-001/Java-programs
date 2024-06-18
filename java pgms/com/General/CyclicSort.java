7b400f66535ccd633bd8b727f614cc2ef43f7227
Subash <subashs10011@gmail.com>
2024-06-18 17:48:15 +0530

    cyclic sort

---
+++ java pgms/com/General/CyclicSort.java
@@ -0,0 +1,35 @@
+package com.General;
+
+import java.util.Arrays;
+// it only valid for elements from 1 to n(which array length)
+public class CyclicSort {
+    public static void main(String[] args) {
+        int[] arr ={5,4,3,1,2};
+        Cyclic(arr);
+        System.out.println(Arrays.toString(arr));
+
+        
+    }
+    static void Cyclic(int[] arr){
+        int i=0;
+        while(i<arr.length){
+            int correctIndex = arr[i]-1;
+            if(arr[i]>0 && arr[i]<= arr.length && arr[i]!= arr[correctIndex]){
+                swap(arr, i, correctIndex);
+            }
+            else{
+                i++;
+            }
+
+        }
+
+    }
+    static void swap(int[] arr, int first, int second){
+        int temp= arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+
+
+}
