7ff2df9d5a0f4d839dcc649ec9b5209ed55f6b2a
Subash <subashs10011@gmail.com>
2026-02-21 14:03:17 +0530

    .

--- java pgms/com/Arrays/RepeatingNumber.java
+++ java pgms/com/Arrays/RepeatingNumber.java
@@ -1,3 +1,26 @@
+import java.util.*;
+
+
 public class RepeatingNumber {
-      
+      public static void main(String[] args) {
+        int[] arr = {1,2,3,3,4,5};
+        Arrays.sort(arr);
+        System.out.println(repeat(arr));
+       
+      }
+      static boolean repeat(int[] arr){
+
+         Set<Integer> set = new HashSet<>();
+        for(int i : arr){
+            if(set.contains(i)){
+                return false;
+            }
+            set.add(i);
+        }
+        System.out.println(set);
+        return true;
+      }
+
+
+
 }
