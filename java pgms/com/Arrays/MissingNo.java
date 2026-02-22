f9d0c56b22de6b4d9a8e1b05a93d0a97122defc5
Subash <subashs10011@gmail.com>
2026-02-22 21:11:11 +0530

    .

--- java pgms/com/Arrays/MissingNo.java
+++ java pgms/com/Arrays/MissingNo.java
@@ -1,7 +1,7 @@
 public class MissingNo {
     public static void main(String[] args) {
         int[] arr ={1,2,3,5,6,7};
-        int n = 7;
+        int n = arr[arr.length-1];
         int expectedSum = n*(n+1)/2;
         int actualSum = 0;
         for(int i : arr){
