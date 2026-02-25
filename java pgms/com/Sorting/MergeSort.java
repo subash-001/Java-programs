0044c99fe70de645179167e9cdd00e9a19188d68
Subash <subashs10011@gmail.com>
2026-02-25 13:59:14 +0530

    merge sort

--- java pgms/com/Sorting/MergeSort.java
+++ java pgms/com/Sorting/MergeSort.java
@@ -1,4 +1,61 @@
 package com.Sorting;
+
+import java.util.Arrays;
+
 public class MergeSort {
-    
+    public static void main(String[] args) {
+        int[] arr={2,3,7,1,9,5};
+        mergeSorting(arr, 0, arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void mergeSorting(int[] arr, int l, int r) {
+       if(l<r){
+        int mid = (l+r)/2;
+        mergeSorting(arr, l,mid);
+        mergeSorting(arr, mid+1, r);
+        merge(arr,l,mid,r);
+       }
+
+    }
+
+    private static void merge(int[] arr, int l, int mid, int r) {
+        int n1= mid - l +1;
+        int n2= r - mid;
+        int[] lArr= new int[n1];
+        int[] rArr= new int[n2];
+        for(int x=0;x<n1;x++){
+            lArr[x]= arr[l+x];
+        }
+        for(int x=0;x<n2;x++){
+            rArr[x]= arr[mid+1+x];
+        }
+        int i=0;
+        int j=0;
+        int k=l;
+        while(i<n1 && j<n2){
+            if(lArr[i] <= rArr[j]){
+                arr[k] = lArr[i];
+                i++;
+            }
+            else{
+                arr[k] = rArr[j];
+                j++;
+            }
+            k++;
+
+           
+        }
+         while(i<n1){
+                arr[k] = lArr[i];
+                i++;
+                k++;
+            }
+            while(j<n2){
+                arr[k] = rArr[j];
+                j++;
+                k++;
+            }
+
+    }
 }
